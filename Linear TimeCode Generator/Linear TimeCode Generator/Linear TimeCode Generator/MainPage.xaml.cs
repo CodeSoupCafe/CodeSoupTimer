689dfa28caf90155d8578ccea2ccac6a1fa832ca
Jeff Holcombe <iknowcodesoup@gmail.com>
2021-01-17 15:05:38 -0500

    base code

---
+++ Linear TimeCode Generator/Linear TimeCode Generator/Linear TimeCode Generator/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Linear_TimeCode_Generator
+{
+  using Xamarin.Forms;
+
+  public partial class MainPage : ContentPage
+  {
+    public MainPage()
+    {
+      InitializeComponent();
+      //LTCSharp.Encoder
+    }
+  }
+}
